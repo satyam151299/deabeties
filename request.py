5118921443e21e489ed51865298701970673eb55
satyam151299 <62535402+satyam151299@users.noreply.github.com>
2020-07-16 15:33:40 +0530

    Create request.py

---
+++ request.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'http://localhost:5000/predict'
+r = requests.post(url,json={'Pregnancies':0, 'Glucose':90, 'BloodPressure':126,'SkinThickness':122,'Insulin':100,'BMI':90,'DiabetesPedigreeFunction':2,'age':40)})
+
+print(r.json())
